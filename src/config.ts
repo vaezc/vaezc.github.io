adff0a46f2b6217420fb008d8406fd947226ea39
zhouchang <xiaochangvv@gmail.com>
2025-02-11 16:58:34 +0800

    初始化

--- src/config.ts
+++ src/config.ts
@@ -4,12 +4,13 @@ import type {
   ProfileConfig,
   SiteConfig,
 } from './types/config'
+
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  title: 'vae的小站',
+  subtitle: '记录生活，记录工作，记录学习',
+  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
@@ -44,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/vaezc',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
@@ -70,7 +71,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/vaezc',
     },
   ],
 }
---
+++ src/content/posts/first.md
@@ -0,0 +1,16 @@
+---
+title: first
+published: 2025-02-11
+description: ""
+image: ""
+tags: []
+category: ""
+draft: false
+lang: ""
+---
+
+# 第一篇博客
+
+## 前言
+
+## 正文
